1a43ee778225a24e7130d6609b68ff927e505421
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2025-10-09 23:04:47 +0100

    Camera Swapping
    Added in functioning camera swapping mechanic to main scene, all cameras are connected to the CameraControllerNode

---
+++ basic-platform-game/Camera_Controller.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var CameraArray:Array[Camera3D] = []
+var currentCamera:Camera3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	CameraArray.append($XCamera)
+	CameraArray.append($YCamera)
+	CameraArray.append($ZCamera)
+	currentCamera = CameraArray[0]
+	pass # Replace with function body.
+	
+
+func _deactivate_camera(current: Camera3D):
+	current.current = false
+
+func _active_camera(index: int):
+	CameraArray[index].current = true
+	var actived_camera = CameraArray[index]
+	return actived_camera
+
+func _input(event) -> void:
+	if event.is_action_pressed("Xcamera"):
+		_deactivate_camera(currentCamera)
+		_active_camera(0)
+		currentCamera = CameraArray[0]
+	if event.is_action_pressed("Ycamera"):
+		_deactivate_camera(currentCamera)
+		_active_camera(1)
+		currentCamera = CameraArray[1]
+	if event.is_action_pressed("Zcamera"):
+		_deactivate_camera(currentCamera)
+		_active_camera(2)
+		currentCamera = CameraArray[2]
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
